49475e36353fb41a2437bd1e6812533b9b045421
Azhang9328 <azhang9328@gmail.com>
2019-12-20 14:11:24 -0800

    Done.

---
+++ db/migrate/005_add_to_characters.rb
@@ -0,0 +1,7 @@
+class AddToCharacters < ActiveRecord::Migration[4.2]
+    def change
+        add_column(:characters, :actor_id, :integer)
+        add_column(:characters, :show_id, :integer)
+        add_column(:characters, :catchphrase, :string)
+    end
+end
