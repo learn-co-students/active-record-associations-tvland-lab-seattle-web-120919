b023f05b2a4375a65c04dd86d2c6f93a9ccd59b0
Cole Rau <colevrau@gmail.com>
2019-12-27 18:27:51 -0800

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name 
+        "#{self.first_name} #{self.last_name}" 
+    end 
+
+    def list_roles
+        array1 = []
+        array2 = []
+        array1 << self.characters[0].name 
+        array2 << self.shows[0].name
+        "#{array1[0]} - #{array2[0]}"
+    end 
 end
+
+
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters 
+    has_many :actors, through: :characters 
+    belongs_to :network 
+
+    def actors_list
+        first_name = self.actors[0].first_name 
+        last_name = self.actors[0].last_name
+        array = []
+        array << first_name + " " + last_name
+    end 
 end
+
